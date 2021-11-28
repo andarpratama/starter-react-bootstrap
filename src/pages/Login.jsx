12ac9c1d2af31a01b39106cd8792688616773651
andarpratama <master.amarta@gmail.com>
2021-11-28 22:07:56 +0700

    simple title dynamicly

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,23 @@
+import React from "react"
 
+class Login extends React.Component {
 
-const Login = () => {
-    return(
-        <div className="container py-4 mt-5">
-            <h2>Welcome to <b>Starter React Bootstrap</b></h2>
-            <h3>Login</h3>
-        </div>
-    )
+    state = {
+        title: 'Login'
+    }
+
+    componentDidMount(){
+        document.title = this.state.title
+    }
+
+    render(){
+        return(
+            <div className="container py-4 mt-5">
+                <h2>Welcome to <b>Starter React Bootstrap</b></h2>
+                <h3>Login</h3>
+            </div>
+        )
+    }
 }
 
 export default Login
